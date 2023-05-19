8d214ed30ee8984b6b7e3d877e523afb6c4f61d4
Mouhamed Gueye <mouhamedgueye@Mouhameds-MacBook-Pro.local>
2023-05-19 15:17:24 +0100

    first ccommit

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -3,16 +3,32 @@ import { PagesList } from './PagesList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
+import Page from './Page';
 
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [selectedPage, setSelectedPage] = useState(null)
+	const [currentPage, setCurrentPage] = useState(null)
+	
 
 	async function fetchPages(){
 		try {
 			const response = await fetch(`${apiURL}/wiki`);
 			const pagesData = await response.json();
+			//getting the data from fetch and setting it to the pages variable
 			setPages(pagesData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+	async function fetchPages(slug){
+		try {
+			const response = await fetch(`${apiURL}/wiki/${slug}`);
+			const pageData = await response.json();
+			setSelectedPage(pageData);
+			console.log(pageData)
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
@@ -22,11 +38,36 @@ export const App = () => {
 		fetchPages();
 	}, []);
 
+	async function handlePageClick (e) {
+		console.log(e.target.value)
+	}
+	const handlePageClick = () = {
+		setSelectedPage
+	}
+
 	return (
 		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+		  <h1>WikiVerse</h1>
+		  {!currentPage ? (
+			<>
+			  <h2>An interesting 📚</h2>
+			  <PagesList pages={pages} onPageClick={handlePageClick} />
+			</>
+		  ) : (
+			<>
+			  <button onClick={handleBackToListClick}>Back to Wiki List</button>
+			  <Page setCurrentPage={setCurrentPage} currentPage={currentPage} />
+			</>
+		  )}
 		</main>
-	)
+	);
+
+	/*return (
+        <main>  
+      <h1>WikiVerse</h1>
+            <h2>An interesting 📚</h2>
+            <PagesList pages={pages} />
+        </main>
+    )*/
+
 }
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export const Page = (props) => {
+  const handleBackToListClick = () => {
+    props.setCurrentPage(null)
+  }
 
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
+
+  return (
+    <>
+      <h3>{props.currentPage.title}</h3>
+      <p>{props.currentPage.content}</p>
+      <ul>
+        {props.currentPage.tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+      <p>{props.page.createdAt}</p>
+      <button onClick={handleBackToListClick}>{props.page.title}</button>
+    </>
+  );
 } 
 	
+
+
+
+export default Page
